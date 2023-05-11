112dc9ba017ec902d7c989c79f432c39a35ca491
skwaiuc <114259266+skwaiuc@users.noreply.github.com>
2023-05-10 23:20:26 -0700

    added preempt functions

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -13,11 +13,14 @@
  * Frequency of preemption
  * 100Hz is 100 times per second
  */
-#define HZ 100
+#define HZ 1000000
+struct sigaction old_action;
+struct itimerval timer;
+
 
 void sighandler(int signum){
 	if (signum == SIGVTALRM){
-		printf("sig\n");
+		//printf("sig\n");
 		uthread_yield();
 	}
 }
@@ -25,11 +28,20 @@ void sighandler(int signum){
 void preempt_disable(void)
 {
 	/* TODO Phase 4 */
+	sigset_t blockSignal;
+	sigemptyset(&blockSignal);
+	sigaddset(&blockSignal, SIGVTALRM);
+	pthread_sigmask(SIG_BLOCK, &blockSignal, NULL);
 }
 
 void preempt_enable(void)
 {
 	/* TODO Phase 4 */
+	sigset_t blockSignal;
+	sigemptyset(&blockSignal);
+	sigaddset(&blockSignal, SIGVTALRM);
+	pthread_sigmask(SIG_UNBLOCK, &blockSignal, NULL);
+
 }
 
 void preempt_start(bool preempt)
@@ -41,21 +53,31 @@ void preempt_start(bool preempt)
 	}
 	
 	struct sigaction action;
-	struct itimerval timer;
+	action.sa_handler = sighandler;
+
+	sigaction(SIGVTALRM, &action, &old_action);
 	timer.it_interval.tv_sec = 0;
 	timer.it_interval.tv_usec = 0;
 	timer.it_value.tv_sec = 0;
 	timer.it_value.tv_usec = 1000000 / HZ;
-	setitimer(ITIMER_VIRTUAL, &timer, NULL);
+	setitimer(ITIMER_VIRTUAL, &timer, NULL);	
 
 	while (1){
 		getitimer(ITIMER_VIRTUAL, &timer);
-		printf("%ld\n", timer.it_value.tv_usec);
+		//printf("%ld\n", timer.it_value.tv_usec);
 	}
 }
 
 void preempt_stop(void)
 {
 	/* TODO Phase 4 */
+
+    sigaction(SIGVTALRM, &old_action, NULL);
+
+    timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 0;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 0;
+    setitimer(ITIMER_VIRTUAL, &timer, NULL);
 }
 
